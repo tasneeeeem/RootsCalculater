19fc8e8ec84fcb53098211c2c4e469a84a6212d8
andrio <lwm7mad12@gmail.com>
2023-10-05 14:08:30 +0300

    CONTINUE

--- app/src/main/java/com/example/rootscalculater/MainActivity.java
+++ app/src/main/java/com/example/rootscalculater/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.rootscalculater;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
 EditText etA,etB,etC;
  Button btnCalc;
@@ -28,10 +31,37 @@ EditText etA,etB,etC;
         String a=etA.getText().toString();
         String b=etB.getText().toString();
         String c=etC.getText().toString();
-double a,b,c,calc;
-        a=Double.parseDouble(a);
-        b=Double.parseDouble(b);
-        c=Double.parseDouble(c);
-calc=
+
+        if (a.isEmpty() || !TextUtils.isDigitsOnly(a) || (a.isEmpty()) || !TextUtils.isDigitsOnly(a)){
+
+            Toast.makeText(this, "Recheck your data", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (b.isEmpty() || !TextUtils.isDigitsOnly(b) || (b.isEmpty()) || !TextUtils.isDigitsOnly(b)){
+
+            Toast.makeText(this, "Recheck your data", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (c.isEmpty() || !TextUtils.isDigitsOnly(c) || (c.isEmpty()) || !TextUtils.isDigitsOnly(c)){
+
+            Toast.makeText(this, "Recheck your data", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        double a1, b1, c1, calc, cal1, delta;
+        a1 = Double.parseDouble(a);
+        b1 = Double.parseDouble(b);
+        c1 = Double.parseDouble(c);
+        delta = (b1 * b1) - (4 * a1 * c1);
+        if (delta == 0) {
+            calc = (-1 * b1) / (2 * a1);
+            Toast.makeText(this, "Roots are:" + calc, Toast.LENGTH_SHORT).show();
+        } else if (delta > 0) {
+            calc = ((-1 * b1) - Math.sqrt(delta)) / (2 * a1);
+            Toast.makeText(this, "Roots are:" + calc, Toast.LENGTH_SHORT).show();
+            cal1 = ((-1 * b1) + Math.sqrt(delta)) / (2 * a1);
+            Toast.makeText(this, "Roots are:" + cal1, Toast.LENGTH_SHORT).show();
+        } else Toast.makeText(this, "There is no roots", Toast.LENGTH_SHORT).show();
     }
 }
